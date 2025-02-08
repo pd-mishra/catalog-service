89d58fe364e1372a96aa2ce4c877f6d31f815f9c
Priyadarshi <pd.mishra@gmail.com>
2025-02-08 22:10:04 +0530

    first commit

---
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -0,0 +1,15 @@
+package com.polarbookshop.catalogservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String getGreeting() {
+		return "Welcome to the book catalog!";
+	}
+	// adding to commit, again
+
+}
